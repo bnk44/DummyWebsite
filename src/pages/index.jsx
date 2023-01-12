1a269ee80f177c5f65b08bbdb229de0116c96f58
Bandar Alkhuthailah <bandarnawaf44@gmail.com>
2023-01-13 00:30:01 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -214,7 +214,7 @@ export default function Home({ articles }) {
           Playlists rendezvous
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          Share your playlists here and discover other people with similiar tase in music by exploring other people's playlists also!
+          Share your playlists here and discover other people with similiar tase in music by exploring other people playlists also!
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
